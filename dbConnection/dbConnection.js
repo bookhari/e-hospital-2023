a058f943301417756f62022745bc714d31b6a488
anasjavaid94 <uottawabiomedicalsystems@gmail.com>
2023-02-27 02:30:04 -0500

    new changes

--- dbConnection/dbConnection.js
+++ dbConnection/dbConnection.js
@@ -1,16 +1,15 @@
 const mysql = require('mysql');
 
 var db = mysql.createConnection(
-  {
-    host: "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   {
+//     host: "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
-    user: "j6qbx3bgjysst4jr",
-    password: 'mcbsdk2s27ldf37t',
-    database: 'evjygdytdp2ev0d',
-    port: 3306
-}
-
-// 'mysql://j6qbx3bgjysst4jr:mcbsdk2s27ldf37t@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkw2tiuvgv6ufu1z'
+//     user: "j6qbx3bgjysst4jr",
+//     password: 'mcbsdk2s27ldf37t',
+//     database: 'evjygdytdp2ev0d',
+//     port: 3306
+// }
+'mysql://j6qbx3bgjysst4jr:mcbsdk2s27ldf37t@frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkw2tiuvgv6ufu1z'
 )
 
 db.connect(function(err) {
