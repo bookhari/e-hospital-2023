458db4fcd0ba71f6022dbb45d1cd21a5e99c78a4
anasjavaid94 <uottawabiomedicalsystems@gmail.com>
2023-03-21 19:43:10 -0400

    changes

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const fs = require('fs');
 const FormData = require('form-data');
 const memoryStorage = multer.memoryStorage()
 const upload = multer({ storage: memoryStorage })
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000; 
 
-
+/* Please use comments to identify your work  */
 
 var sql = '';
 var crypto = require('crypto')
