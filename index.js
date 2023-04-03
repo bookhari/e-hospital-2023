563c81a105c3d7fb217ff8f77f07b82d3d07d383
Prateek Walia <prateekwalia31@gmail.com>
2023-04-03 16:08:01 -0400

    Updated Index problem for alzheimers

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ app.get('/cancerDetection', (req, res) => {
 */
 
 app.get('/AlzheimersDiagnostics', (req, res) => {
-  res.render("pages/BrainTumorDiagnostics");
+  res.render("pages/AlzheimersDiagnosticsDiagnostics");
 })
 
 app.get('/BrainTumorDiagnostics', (req, res) => {
