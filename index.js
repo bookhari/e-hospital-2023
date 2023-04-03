3677c8141430d22c1b3f9096b66dfbc4b8945c4b
anasjavaid94 <uottawabiomedicalsystems@gmail.com>
2023-04-03 01:42:53 -0400

    new changes

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.get('/depressionQuestionnaire', (req, res) => {
 
 
 app.get('/liver', (req, res) => {
-  res.render("pages/liver");
+  res.render("pages/liver-prediction");
 })
 app.get('/liver2', (req, res) => {
   res.render("pages/liver-direct-prediction");
